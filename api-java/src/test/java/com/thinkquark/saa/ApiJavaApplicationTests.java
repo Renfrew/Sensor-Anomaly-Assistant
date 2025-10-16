efabec256b4afae9d3b4c740f36aa4e9b3a01758
Renfrew Chen <Renfrew_world@hotmail.com>
2025-10-16 16:02:58 -0400

    Fix: test container setup on the github

--- api-java/src/test/java/com/thinkquark/saa/ApiJavaApplicationTests.java
+++ api-java/src/test/java/com/thinkquark/saa/ApiJavaApplicationTests.java
@@ -1,18 +1,35 @@
 package com.thinkquark.saa;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@ActiveProfiles("test")
 class ApiJavaApplicationTests {
+
+	@Autowired
+	private Environment env;
+
 	@Container
 	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine");
+
+	@PostConstruct
+	void logProfile() {
+		System.out.println("Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+	}
 
 	@DynamicPropertySource
 	static void registerProps(DynamicPropertyRegistry r) {
